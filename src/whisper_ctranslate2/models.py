1e21b84e34d000ba164e183b7cedc97b66d6227e
Jordi Mas <jmas@softcatala.org>
2023-03-24 15:24:28 +0100

    Include tokenizer.json when downloading models then it can work fully offline

--- src/whisper_ctranslate2/models.py
+++ src/whisper_ctranslate2/models.py
@@ -9,18 +9,18 @@ from tqdm import tqdm
 
 class Models:
     _MODELS = {
-        "tiny": "https://huggingface.co/jordimas/whisper-ct2/resolve/main/1e2aa48a43d45b9304a22aca730c5b5177a923f50008bffd26140e3b6ff19fdf.tiny/",
-        "tiny.en": "https://huggingface.co/jordimas/whisper-ct2/resolve/main/d82547957fb5dffbe279d22292980ab3ad33675a231faadbc9f2c908b909b42d.tiny-en/",
-        "base": "https://huggingface.co/jordimas/whisper-ct2/resolve/main/2b3a5459a344d5cfc6959018fb01f78ad610460d184ddf6a3282c187e7ddeceb.base/",
-        "base.en": "https://huggingface.co/jordimas/whisper-ct2/resolve/main/67b9ae74793ba78292c0c11db4bf296169bbfbc8bd9f81dadf22546b9fa0e809.base-en/",
-        "small": "https://huggingface.co/jordimas/whisper-ct2/resolve/main/64c8c26522e710cfad21c44c037a806cccf0dda756ce0a222b5f3a938d87de43.small/",
-        "small.en": "https://huggingface.co/jordimas/whisper-ct2/resolve/main/ee51a5c0132eb205931bfdd635fae2ada72f5580c812ab113ca2d735ea2707e9.small-en/",
-        "medium": "https://huggingface.co/jordimas/whisper-ct2/resolve/main/309bb6f6fc40dedef3526916b4110288796ca6a873ed73dfc9f3b7bbbf5ecccd.medium/",
-        "medium.en": "https://huggingface.co/jordimas/whisper-ct2/resolve/main/4f2c05f42abebfd9f5f9027e26966153b38995722a066ee8da14e6dbdc976622.medium-en/",
-        "large-v2": "https://huggingface.co/jordimas/whisper-ct2/resolve/main/d2bb7bcdf195976460f6ba94f4222d3ba8d6b306d0a7d98447f3d58eecac4a28.large/",
+        "tiny": "https://huggingface.co/datasets/jordimas/whisper-ct2-v2/resolve/main/dc6a7765cdc8ae7822b1c3068a2f966eddc2549eda7e67406cae915a8c19430c.tiny/",
+        "tiny.en": "https://huggingface.co/datasets/jordimas/whisper-ct2-v2/resolve/main/c4d1be0a2003ce00bb721abd23e7a34925a6f0c0d21d5c416f11c763ee7f7b15.tiny-en/",
+        "base": "https://huggingface.co/datasets/jordimas/whisper-ct2-v2/resolve/main/d7c4df31737340263ce37933bda1e77a38367dbb09cda7433ce1ee0c58ce1a60.base/",
+        "base.en": "https://huggingface.co/datasets/jordimas/whisper-ct2-v2/resolve/main/2ca96777261aeadd48e30bc5f26fd3ee462f4921dbc1f38dfd826a67c761c9b2.base-en/",
+        "small": "https://huggingface.co/datasets/jordimas/whisper-ct2-v2/resolve/main/936cd99363be80fa388c0c5006e846d8cd42834c6cf8156a7c300723a3bf929f.small/",
+        "small.en": "https://huggingface.co/datasets/jordimas/whisper-ct2-v2/resolve/main/e2c14bb0f6a8a69afe12fbe1d82fa0c41494d4bde9615bdf399da5665f43cbc4.small-en/",
+        "medium": "https://huggingface.co/datasets/jordimas/whisper-ct2-v2/resolve/main/d8b91e278db3041c3b41bf879716281edf5cfa7b0025823cc174b5429877d2bc.medium/",
+        "medium.en": "https://huggingface.co/datasets/jordimas/whisper-ct2-v2/resolve/main/22d42d3e69ce9149bfd52c07d357e4cb72b992fb602805d6bb39f331400d6742.mediu-en/",
+        "large-v2": "https://huggingface.co/datasets/jordimas/whisper-ct2-v2/resolve/main/ea44e7a9609bd21a604b28880b85fc7dc2c373876c627a7553ce5440a8c406c1.large/",
     }
 
-    _FILES = ["model.bin", "config.json", "vocabulary.txt"]
+    _FILES = ["model.bin", "config.json", "vocabulary.txt", "tokenizer.json"]
 
     def __init__(self):
         default = os.path.join(os.path.expanduser("~"), ".cache")
