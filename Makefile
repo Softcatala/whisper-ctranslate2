ed23010ac1faf81df1354c41b72499654ae69378
Jordi Mas <jmas@softcatala.org>
2023-04-26 17:34:09 +0200

    Add initial UT for the writers

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-.PHONY: run run-e2e-test
+.PHONY: run run-e2e-test run-tests
 
 run:
 	python3 setup.py sdist bdist_wheel
 	pip3 install --force-reinstall --user .
 	
-run-e2e-test:
+run-e2e-tests:
 	pip install --force-reinstall ctranslate2==3.12
 	pip install --force-reinstall faster-whisper==0.5.0
-	CT2_USE_MKL="False" CT2_FORCE_CPU_ISA='GENERIC' nose2
+	CT2_USE_MKL="False" CT2_FORCE_CPU_ISA='GENERIC' nose2 -s e2e-tests
+
+run-tests:
+	nose2 -s tests
---
+++ tests/testwriters.py
@@ -0,0 +1,93 @@
+import unittest
+import os
+from src.whisper_ctranslate2.writers import WriteTXT, WriteSRT
+from tempfile import NamedTemporaryFile
+from faster_whisper.transcribe import Segment, Word
+
+
+class TestCmd(unittest.TestCase):
+    def _get_segment(self, text, start=0, end=1):
+        return Segment(
+            start=start,
+            end=end,
+            text=text,
+            words=[],
+            avg_logprob=0,
+            no_speech_prob=0,
+            id=0,
+            seek=0,
+            tokens=[],
+            temperature=0,
+            compression_ratio=0,
+        )._asdict()
+
+    def _read_subtitles(self, filename):
+        with open(filename, "r") as fh_r:
+            return fh_r.readlines()
+
+    def _get_temp_file_name_dir(self):
+        f = NamedTemporaryFile()
+        return f.name, os.path.dirname(f.name)
+
+    def test_writetext(self):
+        segments = [
+            self._get_segment("Hello my friends."),
+            self._get_segment("How are you?"),
+        ]
+        results = {"text": "all text", "segments": segments}
+
+        filename, dirname = self._get_temp_file_name_dir()
+        subtitlesWriter = WriteTXT(output_dir=dirname)
+        subtitlesWriter(results, filename)
+
+        r = self._read_subtitles(filename + ".txt")
+
+        self.assertEqual(2, len(r), "len")
+        self.assertEqual("Hello my friends.\n", r[0], "text")
+        self.assertEqual("How are you?\n", r[1], "text")
+
+    def test_writesrt(self):
+        segments = [
+            self._get_segment("Hello my friends.", start=1, end=5),
+            self._get_segment("How are you?", start=6.5, end=8),
+        ]
+        results = {"text": "all text", "segments": segments}
+
+        filename, dirname = self._get_temp_file_name_dir()
+        subtitlesWriter = WriteSRT(output_dir=dirname)
+        subtitlesWriter(results, filename)
+        r = self._read_subtitles(filename + ".srt")
+
+        self.assertEqual(8, len(r), "text")
+        self.assertEqual("1\n", r[0], "text")
+        self.assertEqual("00:00:01,000 --> 00:00:05,000\n", r[1], "text")
+        self.assertEqual("Hello my friends.\n", r[2], "text")
+        self.assertEqual("\n", r[3], "text")
+        self.assertEqual("2\n", r[4], "text")
+        self.assertEqual("00:00:06,500 --> 00:00:08,000\n", r[5], "text")
+        self.assertEqual("How are you?\n", r[6], "text")
+        self.assertEqual("\n", r[7], "text")
+
+    def test_writesrt_words(self):
+        segment = self._get_segment("Hello my friends.", start=1, end=5)
+        segments = [segment]
+        segments[0]["words"] = [
+            Word(start=1, end=2, word="Hello", probability=0)._asdict(),
+        ]
+
+        results = {"text": "all text", "segments": segments}
+
+        filename, dirname = self._get_temp_file_name_dir()
+        subtitlesWriter = WriteSRT(output_dir=dirname)
+        subtitlesWriter(results, filename)
+        r = self._read_subtitles(filename + ".srt")
+        print(r)
+
+        self.assertEqual(8, len(r), "text")
+        self.assertEqual("1\n", r[0], "text")
+        self.assertEqual("00:00:01,000 --> 00:00:02,000\n", r[1], "text")
+        self.assertEqual("<u>Hello</u>\n", r[2], "text")
+
+
+if __name__ == "__main__":
+    unittest.main()
