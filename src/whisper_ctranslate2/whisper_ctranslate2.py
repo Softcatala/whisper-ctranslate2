24f32291b87a12daf103db786c8e6483a64f4d95
foul11 <artur200320081@gmail.com>
2023-04-29 10:28:46 +0700

    add select live device

--- src/whisper_ctranslate2/whisper_ctranslate2.py
+++ src/whisper_ctranslate2/whisper_ctranslate2.py
@@ -330,6 +330,13 @@ def read_command_line():
         default=0.2,
         help="minimum volume threshold to activate listening in live transcribe mode",
     )
+    
+    live_args.add_argument(
+        "--live_device",
+        type=int,
+        default=None,
+        help="Set live stream input device (python -m sounddevice)",
+    )
 
     return parser.parse_args().__dict__
 
@@ -363,6 +370,7 @@ def main():
     local_files_only: bool = args.pop("local_files_only")
     highlight_words: bool = args.pop("highlight_words")
     live_volume_threshold: float = args.pop("live_volume_threshold")
+    live_device: int = args.pop('live_device')
 
     temperature = args.pop("temperature")
     if (increment := args.pop("temperature_increment_on_fallback")) is not None:
@@ -468,6 +476,7 @@ def main():
             compute_type,
             verbose,
             live_volume_threshold,
+            live_device,
             options,
         ).inference()
 
