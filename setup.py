3f0295d7d18741683bd8e07ea5508084b470a88a
Jordi Mas <jmas@softcatala.org>
2023-10-14 19:08:46 +0200

    Add missing comma to separate classifiers

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12"
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     packages=["src/whisper_ctranslate2"],
