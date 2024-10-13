e79504da1308c5d8dfd27e42d2cac7422e17dc9a
Jordi Mas <jmas@softcatala.org>
2024-10-13 16:51:11 +0200

    Include Python 3.13 in CI and in application metadata

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     packages=["src/whisper_ctranslate2"],
